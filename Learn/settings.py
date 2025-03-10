c2403dae36e386d396372c9335e7c87b8b9e8331
DenKlyzi <denis.zinovkin234@gmail.com>
2025-03-10 10:22:00 +0300

    commit

--- Learn/settings.py
+++ Learn/settings.py
@@ -123,4 +123,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = os.path.join(BASE_DIR)
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
